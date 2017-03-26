73bc0c861329d2dda62207f4988d5923465da851
Joël Goossens <j.a.j.goossens@student.tue.nl>
2017-03-26 18:19:56 +0200

    Meer bugfixings, nu van 500x784 naar 500xk zoals t hoort

--- Assignment3Task1.py
+++ Assignment3Task1.py
@@ -32,11 +32,11 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=30, train
 k=45
 
 #D is the number of dimensions in the original dataset
-d=500
+d=784
 
-rMatrix = [[0 for aaaaa in range(d)] for bbbbb in range(k)]
-for i in range(0,k):
-    for j in range(0,d):
+rMatrix = [[0 for aaaaa in range(k)] for bbbbb in range(d)]
+for i in range(0,d):
+    for j in range(0,k):
         coinFlip = np.random.randint(0,2)
         if (coinFlip == 0):
             rMatrix[i][j] = 1/d
@@ -44,15 +44,18 @@ for i in range(0,k):
             rMatrix[i][j] = -(1/d)
 
 
-fP= np.dot(rMatrix,X)
-
-zouHet = 255*np.array(rMatrix)
+fP= np.dot(X,rMatrix)
 
 p = np.amax(X)
 q = np.amax(y)
 
-onmogelijkeShit = abs(p*np.array(fP) - q*np.array(fP))/ abs(p - q)
+randProjX = abs(p*np.array(fP) - q*np.array(fP))/ abs(p - q)
 
+plt.hist(randProjX)
+plt.show()
+
+print(np.amax(randProjX))
+#
 # ###Part 2
 # kValues = [45,90,400]
 #
@@ -60,27 +63,32 @@ onmogelijkeShit = abs(p*np.array(fP) - q*np.array(fP))/ abs(p - q)
 # transformedAccuracies = []
 #
 # # loop over the projection sizes
-# for comp in kValues:
-#
-#     # train a classifier on the initial values
-#     originalModel = KNeighborsClassifier(n_neighbors=1, n_jobs=-1)
-#     originalModel.fit(X_train, y_train)
-#     # evaluate the model and update the list of accuracies
-#     originalAccuracies.append(metrics.accuracy_score(originalModel.predict(X_test), y_test))
 #
 #
-#     # create the random projection
-#     sp = SparseRandomProjection(n_components=comp)
-#     X_trainFit= sp.fit_transform(X_train)
-#     # train a classifier on the sparse random projection
-#     rpModel = KNeighborsClassifier(n_neighbors=1, n_jobs=-1)
-#     rpModel.fit(X_trainFit, y_train)
-#     # evaluate the model and update the list of accuracies
-#     X_testTrans = sp.transform(X_test)
-#     transformedAccuracies.append(metrics.accuracy_score(rpModel.predict(X_testTrans), y_test))
+# # train a classifier on the initial values
+# originalModel = KNeighborsClassifier(n_neighbors=1, n_jobs=-1)
+# originalModel.fit(X_train, y_train)
+# # evaluate the model and update the list of accuracies
+# originalAccuracies.append(metrics.accuracy_score(originalModel.predict(X_test), y_test))
 #
-# # #Check without random projection
-# # nn = KNeighborsClassifier(n_neighbors=1, n_jobs=-1)
-# # nn.fit(X_train,y_train)
-# # print("Training set score: {:.3f}".format(nn.score(X_train, y_train)))
-# # print("Test set score: {:.3f}".format(nn.score(X_test, y_test)))
+#
+# # create the random projection
+# X_trainRand, X_testRand, y_trainRand, y_testRand = train_test_split(randProjX, y, random_state=30, train_size=0.8)
+# # train a classifier on the sparse random projection
+# rpModel = KNeighborsClassifier(n_neighbors=1, n_jobs=-1)
+# rpModel.fit(X_trainRand, y_trainRand)
+# # evaluate the model and update the list of accuracies
+# transformedAccuracies.append(metrics.accuracy_score(rpModel.predict(X_testRand), y_testRand))
+#
+# print("Original accuracy sores are:")
+# print(originalAccuracies)
+# print("Accuracy scores using random projection are:")
+# print(transformedAccuracies)
+#
+#
+# #
+# # # #Check without random projection
+# # # nn = KNeighborsClassifier(n_neighbors=1, n_jobs=-1)
+# # # nn.fit(X_train,y_train)
+# # # print("Training set score: {:.3f}".format(nn.score(X_train, y_train)))
+# # # print("Test set score: {:.3f}".format(nn.score(X_test, y_test)))
